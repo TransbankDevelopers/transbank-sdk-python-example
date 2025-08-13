c25d5e870fad5cf69e600e50d66d65ce9659184e
victor mendoza <victor.mendoza@continuumhq.com>
2025-08-13 13:20:17 -0300

    fix: delete comment

--- webpay_plus/views.py
+++ webpay_plus/views.py
@@ -6,7 +6,7 @@ from transbank.webpay.webpay_plus.transaction import Transaction
 from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
 from transbank.common.integration_api_keys import IntegrationApiKeys
 
-# Create your views here.
+
 def get_transbank_transaction():
     return Transaction.build_for_integration(
         IntegrationCommerceCodes.WEBPAY_PLUS,
@@ -27,8 +27,7 @@ def create(request):
         
         context = {
             'request_data': {'buy_order': buy_order, 'session_id': session_id, 'amount': amount},
-            'respond_data': resp,
-            'hola': "hola"
+            'respond_data': resp
         }
         return render(request, 'webpay_plus/create.html', context)
        
