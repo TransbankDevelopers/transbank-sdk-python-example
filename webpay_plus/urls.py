0aa79b4a72acbad389cb64dac6612e4d3df13ccb
mvarlic <mvarlic@gmail.com>
2025-08-20 13:29:31 -0500

    feat: add webpay normal methods

--- webpay_plus/urls.py
+++ webpay_plus/urls.py
@@ -5,4 +5,7 @@ app_name = 'webpay_plus'
 
 urlpatterns = [
     path('', views.create, name='create'),
+    path('commit', views.commit, name='commit'),
+    path('refund', views.refund, name='refund'),
+    path('status', views.status, name='status'),
 ]
--- webpay_plus/views.py
+++ webpay_plus/views.py
@@ -1,11 +1,17 @@
 import random
+import logging
+
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
 from transbank.webpay.webpay_plus.transaction import Transaction
 from transbank.common.integration_commerce_codes import IntegrationCommerceCodes
 from transbank.common.integration_api_keys import IntegrationApiKeys
 
+logging.basicConfig(level=logging.INFO)  
+logger = logging.getLogger(__name__)
 
 def get_transbank_transaction():
     return Transaction.build_for_integration(
@@ -23,37 +29,87 @@ def create(request):
         "example": "Ejemplo",
     }
 
-    
-    snippet_request_php = """\
-    use Transbank\\Webpay\\WebpayPlus\\Transaction;
-    use Transbank\\Webpay\\Options;
-    // configuración de la transacción
-    $option = new Options(API_KEY, COMMERCE_CODE, Options::ENVIRONMENT_INTEGRATION);
-    $transaction = new Transaction($option);
-    $response = $transaction->create($buyOrder, $sessionId, $amount, $returnUrl);
-    """
     try:    
-       
-
         tx = get_transbank_transaction()
-        buy_order = f"O-{random.randint(1, 10000)}"
-        session_id = f"S-{random.randint(1, 10000)}"
-        amount = random.randint(1000, 2000)
-        return_url = "https://www.google.com"
-        
-        resp = tx.create(buy_order, session_id, amount, return_url)
-        
+        create_tx = {
+            'buy_order': "O-" + str(random.randint(1, 10000)),
+            'session_id': "S-" + str(random.randint(1, 10000)),
+            'return_url': request.build_absolute_uri("/webpay-plus/commit"),
+            'amount': random.randint(1000, 2000),
+        }
+        resp = tx.create(create_tx["buy_order"], create_tx["session_id"], create_tx["amount"], create_tx["return_url"])
+
         context = {
             "active_link": "Webpay Plus",
             "navigation": navigation,
-            "snippet_request_php": snippet_request_php,
-            
-            
-            'respond_data': resp
+            "request": create_tx,
+            'response_data': resp
         }
       
         return render(request, 'webpay_plus/create.html', context)
        
     except Exception as e:
-       
         return render(request, "webpay_plus/create.html", {'error': str(e)})
+
+
+@csrf_exempt  
+def commit(request):
+    tx = get_transbank_transaction()
+    try:
+        view = "error/webpay/timeout.html"
+        data = {"request": request}
+        tbk_token = request.GET.get("TBK_TOKEN") or request.POST.get("TBK_TOKEN")
+        token_ws = request.GET.get("token_ws") or request.POST.get("token_ws")
+
+        if tbk_token and token_ws:
+            view = "error/webpay/form_error.html"
+        elif tbk_token:
+            view = "error/webpay/aborted.html"
+            resp = tx.status(tbk_token)
+            data["response_data"] = resp
+        elif token_ws:
+            resp = tx.commit(token_ws)
+            view = "webpay_plus/commit.html"
+            data = {
+                "response_data": resp,
+                "token": token_ws,
+                "returnUrl": request.build_absolute_uri("/webpay_plus/commit"),
+            }
+
+        return render(request, view, data)
+
+    except Exception as e:
+        return render(request, "error.html", {"error": str(e)})
+
+
+@csrf_exempt
+@require_GET
+def refund(request):
+    tx = get_transbank_transaction()
+    try:
+        token = request.GET.get("token")
+        amount = request.GET.get("amount")
+        resp = tx.refund(token, amount)
+
+        return render(
+            request, "webpay_plus/refund.html", {"response_data": resp, "token": token}
+        )
+
+    except Exception as e:
+        return render(request, "error_page.html", {"error": str(e)})
+
+
+@require_GET
+def status(request):
+    tx = get_transbank_transaction()
+    try:
+        token = request.GET.get("token")
+        resp = tx.status(token)
+
+        return render(
+            request, "webpay_plus/status.html", {"response_data": resp, "req": request, "token": token}
+        )
+
+    except Exception as e:
+        return render(request, "error_page.html", {"error": str(e)})
+    
