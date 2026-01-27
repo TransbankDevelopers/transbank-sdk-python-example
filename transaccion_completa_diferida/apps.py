100a53fd927586360a550a07c5a7bf8bd003ebe6
victor mendoza <victor.mendoza@continuumhq.com>
2026-01-27 13:32:54 -0300

    feat: add transaction completa diferida app

---
+++ transaccion_completa_diferida/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TransaccionCompletaDiferidaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'transaccion_completa_diferida'
