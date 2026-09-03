0326d140082b15573383fedcc454fc1c70954ad9
Mauricio Astudillo Toledo <m.astudillo.toledo@gmail.com>
2026-09-03 20:55:46 +0000

    chore: update post-create script to remove --deploy option from pipenv sync

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 python -m pip install --user pipenv
-"${HOME}/.local/bin/pipenv" sync --system --deploy
+"${HOME}/.local/bin/pipenv" sync --system
 python manage.py migrate --noinput
